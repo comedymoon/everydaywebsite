881ab5d56e3de113c4047a1ffe88de00add5d5ad
comedymoon <kdv.usb@gmail.com>
2025-08-21 11:21:41 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+: "${PORT:=10000}"
+
+# Rewrite Apache to listen on Render's port
+sed -ri "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -ri "s/:80>/:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+
+exec apache2-foreground
